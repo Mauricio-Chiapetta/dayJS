7fb8ddc1b48dad081f8318b8dc66f14c29211f85
Mauricio Chiapetta <m.faisca1234@gmail.com>
2023-03-16 02:51:57 -0300

    meses

--- aniversario.js
+++ aniversario.js
@@ -7,10 +7,12 @@ function birthday (date){
     const ageInYears = today.diff(birthday,'year')
     const nextBirthday = birthday.add(ageInYears+1,"year")
     const daysToNextBirthday = nextBirthday.diff(today,'day')
-    
+    const monthsToTheNextBirthday = nextBirthday.diff(today,'month')+1
+
     console.log(`Idade: ${ageInYears}`)
     console.log(`próximo aniversário: ${nextBirthday.format("DD/MM/YYYY")}`)
-    console.log(`Dias até completar 20 anos: ${daysToNextBirthday} dias`);
+    console.log(`Dias até completar 20 anos: ${daysToNextBirthday} dias`)
+    console.log(`Falta ${monthsToTheNextBirthday} meses para seu próximo aniversário!`);
 }
 // dayjs("2003-07-08").format("08/07/2003")
 birthday("2003-07-08")
